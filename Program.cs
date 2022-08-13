55aa0247284883f2484870b10ead3372e56ff4ed
jyoti <jyotidhage10@gmail.com>
2022-08-13 19:15:53 +0530

    Welcome to Employee wage Computatiom program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeeWageProblems{
+
+    class Program {
+
+        public static void Main(string[] args)
+        {
+            EmployeePresentAbsent employeePresentAbsent = new EmployeePresentAbsent();
+            employeePresentAbsent.EmployeePresentOrAbsent();
+
+            DailyEmployeewages dailyEmpwage = new DailyEmployeewages();
+            dailyEmpwage.EmployeeDailyWage()
+
+            PartTimeEmployeeWages partTimeEmployeeWage = new PartTimeEmployeeWages();
+            partTimeEmployeeWage.PartTimeWage();
+
+            CalculateWages calculateWages = new CalculateWages();
+            calculateWages.calculateHrswages();
+
+            SwitchCaseStatement switchCaseStatement = new SwitchCaseStatement();
+            switchCaseStatement.CaseStatement();
+
+            MonthlyEmpWages monthlyEmpWage = new MonthlyEmpWages();
+            monthlyEmpWage.MontlyWageComputation();
+
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.ComputeWage();
+
+            MultipleCompanies multipleCompanies = new MultipleCompanies();
+            multipleCompanies.ComputeWage();
+
+        }
+
+
+    }
+
+}
